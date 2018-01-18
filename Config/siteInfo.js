873670169b144233f4e18811fed656c08c9ce9b9
sepehr500 <sepehr411@gmail.com>
2018-01-17 19:54:03 -0500

    Added NYT and Fox News

--- Config/siteInfo.js
+++ Config/siteInfo.js
@@ -1,6 +1,14 @@
 module.exports = {
 	breitbart: {
 		name: 'Breitbart',
-		url: 'http://www.breitbart.com' 
+		url: 'http://www.breitbart.com'
+	},
+	NYT: {
+		name: 'New York Times',
+		url: 'https://www.nytimes.com'
+	},
+	FoxNews: {
+		name: 'Fox News',
+		url: 'http://www.foxnews.com'
 	}
 };
