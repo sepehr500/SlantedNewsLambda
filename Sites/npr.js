cbd34f92d3434b473d72139690c0cb6cc40ef702
sepehr500 <sepehr411@gmail.com>
2018-01-21 23:30:52 -0500

    Fixed npr

--- Sites/npr.js
+++ Sites/npr.js
@@ -6,6 +6,6 @@ module.exports = cheerioFetch(siteInfo.npr.url).then(
 	$ => {
 		const node = $('.title').first();
 		const parent = node.parent()[0];
-		return createPayload({name: siteInfo.npr.name, node: parent, url: siteInfo.npr.url, text: node.text()});
+		return createPayload({name: siteInfo.npr.name,link: parent.attribs.href, url: siteInfo.npr.url, text: node.text()});
 	}
 );
