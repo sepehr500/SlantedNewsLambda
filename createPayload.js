cd60c9b1d55b2d88314b3be4fc9296f0a302eebd
Ken Bowden <Ken@kens-mbp.attlocal.net>
2018-02-11 21:12:47 -0500

    Adding CBS

--- createPayload.js
+++ createPayload.js
@@ -2,17 +2,20 @@ var R = require('ramda');
 
 var configureUrl = R.curry((siteUrl, hasLink, link) => { return link.includes(siteUrl) || hasLink ? link : siteUrl + link;});
 
-module.exports = (obj) => {
-	return R.tryCatch(R.compose(
-									R.objOf(obj.name),
-									R.evolve({articleLink: configureUrl(obj.url,obj.link)}),
-									(obj) => {
-										return {
-											articleLink: obj.link ? obj.link : obj.node.attr('href'),
-											articleTitle: obj.text ? obj.text : obj.node.text(),
-											siteUrl: obj.url
-										};
-									}
-								),R.always({}))(obj) ;
+var formatArticleLink = (siteUrl, link) => {
+	return (link.indexOf(siteUrl) >= 0 || link.indexOf('http') >= 0) ? link : `${siteUrl}${link}`;
 };
 
+module.exports = (obj) => {
+	return R.tryCatch(R.compose(
+		R.objOf(obj.name),
+		R.evolve({articleLink: configureUrl(obj.url,obj.link)}),
+		(obj) => {
+			return {
+				articleLink: formatArticleLink(obj.url, obj.link),
+				articleTitle: obj.text ? obj.text : obj.node.text(),
+				siteUrl: obj.url
+			};
+		}
+	),R.always({}))(obj);
+};
