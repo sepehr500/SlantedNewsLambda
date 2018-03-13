225f3cbaad633f4639cc392fe043b236d294cda4
sepehr500 <sepehr411@gmail.com>
2018-03-12 20:14:35 -0400

    Sentiment

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 // For development/testing purposes
-const clusterData = require('./cluster');
-var writeToDynamo = require('./writeToDynamo');
-var	brietbart = require('./Sites/breitbart'),
-	foxNews = require('./Sites/foxNews'),
-	npr = require('./Sites/npr'),
-	nyt = require('./Sites/newYorkTimes'),
-	dailyCaller = require('./Sites/dailyCaller'),
-	drudge = require('./Sites/drudge'),
-	blaze = require('./Sites/theBlaze'),
-	// ap = require('./Sites/ap'),
-	reuters = require('./Sites/reuters'),
-	hill = require('./Sites/theHill'),
-	economist = require('./Sites/theEconomist'),
-	wsj = require('./Sites/wjs'),
-	vox = require('./Sites/vox'),
-	washPo = require('./Sites/washPo'),
-	politico = require('./Sites/politico'),
-	huffPo= require('./Sites/huffPo');
+exports.handler = function(event, context, callback) {
+	const clusterData = require('./cluster');
+	var writeToDynamo = require('./writeToDynamo');
+	var	brietbart = require('./Sites/breitbart'),
+		foxNews = require('./Sites/foxNews'),
+		npr = require('./Sites/npr'),
+		nyt = require('./Sites/newYorkTimes'),
+		dailyCaller = require('./Sites/dailyCaller'),
+		drudge = require('./Sites/drudge'),
+		blaze = require('./Sites/theBlaze'),
+		// ap = require('./Sites/ap'),
+		reuters = require('./Sites/reuters'),
+		hill = require('./Sites/theHill'),
+		economist = require('./Sites/theEconomist'),
+		wsj = require('./Sites/wjs'),
+		vox = require('./Sites/vox'),
+		washPo = require('./Sites/washPo'),
+		politico = require('./Sites/politico'),
+		huffPo= require('./Sites/huffPo');
 	// cbs = require('./Sites/cbs'),
 	// cnn = require('./Sites/cnn');
 
-var promises = [
+	var promises = [
 	// ap,
-	blaze,
-	brietbart,
-	// cbs,
-	dailyCaller,
-	drudge,
-	economist,
-	foxNews, 
-	hill,
-	npr, 
-	nyt,
-	reuters,
-	vox,
-	washPo,
-	wsj,
-	politico,
-	huffPo,
+		blaze,
+		brietbart,
+		// cbs,
+		dailyCaller,
+		drudge,
+		economist,
+		foxNews, 
+		hill,
+		npr, 
+		nyt,
+		reuters,
+		vox,
+		washPo,
+		wsj,
+		politico,
+		huffPo,
 	// cnn
-];
+	];
 
-exports.handler = function(event, context, callback) {
 	Promise.all(promises).then(result => {
 		const mergedObj = result.reduce((acc, cur) => Object.assign(acc, cur), {});
-		// const clustered = clusterData(mergedObj);
-		// console.log(mergedObj);
 		writeToDynamo(mergedObj, callback);
 	}).catch(err => console.log(err));
 };
